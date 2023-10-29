6cd801117bf995d2ad5e06c349c9b37051722715
Fiji404 <radek42310o@gmail.com>
2023-10-29 16:45:16 +0100

    remove files

--- backend/src/middlewares/invoices.middleware.ts
+++
@@ -1,5 +0,0 @@
-// import { NextFunction, Response } from 'express';
-
-// export const InvoicesMiddleware = async (req: any, res: Response, next: NextFunction) => {
-//     console.log(req, res, next);
-// };
