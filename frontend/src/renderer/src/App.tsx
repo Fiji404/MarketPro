a6f22bb3d50e9b22ea819e13384016a38b8e4a2d
Fiji404 <radek42310o@gmail.com>
2023-11-02 22:04:48 +0100

    fix: clean electron files from comments, change formatting

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -1,16 +1,6 @@
-import { useEffect } from 'react';
 import { Navbar } from './components';
 
 export const App = () => {
-    useEffect(() => {
-        const getInvoices = async () => {
-            const res = await fetch('http://localhost:3000/invoices');
-            const data = await res.json();
-            console.log(data);
-        };
-        getInvoices();
-    }, []);
-
     return (
         <>
             <Navbar />
