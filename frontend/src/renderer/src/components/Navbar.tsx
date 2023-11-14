0c06ddad9d8949d60764a44bd5bb2c5a7b368f9b
Fiji404 <radek42310o@gmail.com>
2023-11-14 20:34:46 +0100

    refactor: frontend remove unecessary code, change app icon

--- frontend/src/renderer/src/components/Navbar.tsx
+++ frontend/src/renderer/src/components/Navbar.tsx
@@ -1,34 +1,17 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { socket } from '@renderer/socket';
-
-const NAV_LINKS = [
-    {
-        text: 'Statystyki',
-        href: '/stats'
-    }
-];
-
 export const Navbar = () => {
-    useEffect(() => {
-        socket.on('connect', () => {
-            console.log('Client connected, ID:', socket.id);
-        });
-    }, []);
     return (
         <nav className="flex items-center justify-between text-accent h-16 px-6">
             <h1 className="text-xl font-extrabold text-foreground transition-colors">
                 <Link to="/">MarketPro</Link>
             </h1>
             <ul className="py-2 flex justify-end gap-3">
-                {NAV_LINKS.map(({ text, href }) => (
-                    <li key={text}>
-                        <Link to={href} className="text-base hover:text-foreground transition-colors">
-                            {text}
-                        </Link>
-                    </li>
-                ))}
+                <li>
+                    <Link to="/stats" className="text-base hover:text-foreground transition-colors">
+                        Statystyki
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
--- frontend/src/renderer/src/pages/Stats.tsx
+++ frontend/src/renderer/src/pages/Stats.tsx
@@ -15,7 +15,6 @@ export const Stats = () => {
             const res = await fetch('http://0.0.0.0:3000/products');
             const data = await res.json();
             setProducts(data);
-            console.log(data);
         };
         getProducts();
 
@@ -23,7 +22,7 @@ export const Stats = () => {
     }, []);
 
     return (
-        <main className='px-6'>
+        <main className="px-6">
             <h1 className="mt-5 text-3xl font-extrabold text-foreground text-center">
                 Oto lista wszystkich zakupionych produktów
             </h1>
@@ -44,8 +43,12 @@ export const Stats = () => {
                         products.map(product => {
                             return (
                                 <tr className="flex gap-1 justify-between mb-1 card" key={product.id}>
-                                    <td className="product-detail border-r border-[rgba(255,255,255,0.2)]">{product.name}</td>
-                                    <td className="product-detail border-r border-[rgba(255,255,255,0.2)]">{product.quantity}</td>
+                                    <td className="product-detail border-r border-[rgba(255,255,255,0.2)]">
+                                        {product.name}
+                                    </td>
+                                    <td className="product-detail border-r border-[rgba(255,255,255,0.2)]">
+                                        {product.quantity}
+                                    </td>
                                     <td className="product-detail">{product.netPrice}zł</td>
                                 </tr>
                             );
